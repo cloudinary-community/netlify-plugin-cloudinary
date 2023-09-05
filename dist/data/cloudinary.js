c7b5c056f54ae43eb7805e08aa1f30eb3ade5171
Matias Hernandez <matiasfh@gmail.com>
2023-09-05 10:48:03 -0300

    remove dist from vc

--- dist/data/cloudinary.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PUBLIC_ASSET_PATH = exports.PREFIX = void 0;
-exports.PREFIX = 'cld';
-exports.PUBLIC_ASSET_PATH = `${exports.PREFIX}-assets`;
--- dist/data/errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ERROR_SITE_NAME_REQUIRED = exports.ERROR_NETLIFY_HOST_CLI_SUPPORT = exports.ERROR_NETLIFY_HOST_UNKNOWN = exports.ERROR_CLOUD_NAME_REQUIRED = void 0;
-exports.ERROR_CLOUD_NAME_REQUIRED = 'A Cloudinary Cloud Name is required. Please set cloudName input or use the environment variable CLOUDINARY_CLOUD_NAME';
-exports.ERROR_NETLIFY_HOST_UNKNOWN = 'Cannot determine Netlify host, can not proceed with plugin.';
-exports.ERROR_NETLIFY_HOST_CLI_SUPPORT = 'Note: The Netlify CLI does not currently support the ability to determine the host locally, try deploying on Netlify.';
-exports.ERROR_SITE_NAME_REQUIRED = 'Cannot determine the site name, can not proceed with plugin';
