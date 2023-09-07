870a15e4eecd2d4e1331e8c407f053cc30b7862f
Colby Fayock <fayock@gmail.com>
2023-09-06 21:07:06 -0400

    seo, favicon, 401 error page

--- docs/theme.config.tsx
+++ docs/theme.config.tsx
@@ -16,6 +16,23 @@ const config: DocsThemeConfig = {
       }
     }
   },
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+      <title>Netlify Cloudinary - Automatic Optimization at Scale</title>
+      <meta name="description" content="Optimize and deliver all images in your Netlify site with Cloudinary." />
+      <meta name="og:title" content="Netlify Cloudinary - Automatic Optimization at Scale" />
+      <meta name="og:description" content="Optimize and deliver all images in your Netlify site with Cloudinary." />
+      <meta name="og:type" content="website" />
+
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+      <link rel="manifest" href="/site.webmanifest" />
+      <meta name="theme-color" content="#000000" />
+    </>
+  ),
   footer: {
     text: `MIT ${new Date().getFullYear()} © Colby Fayock`,
   },
