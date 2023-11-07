bedd68fbe5cf1cd0488d48f08b793060934c9ce8
Colby Fayock <colbyfayock@users.noreply.github.com>
2023-11-07 15:35:52 -0500

    Vitest (#92)
    # Description
    
    Migrates from Jest to Vitest
    
    ## Issue Ticket Number
    
    <!-- Specifiy which issue this fixes by referencing the issue number
    (`#11`) or issue URL. -->
    <!-- Example: Fixes
    https://github.com/colbyfayock/netlify-plugin-cloudinary/issues/1 -->
    
    Fixes #90 
    
    ## Type of change
    
    <!-- Please select all options that are applicable. -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to not work as expected)
    - [ ] This change requires a documentation update
    
    
    # Checklist
    
    <!-- These must all be followed and checked. -->
    
    - [ ] I have followed the contributing guidelines of this project as
    mentioned in [CONTRIBUTING.md](/CONTRIBUTING.md)
    - [ ] I have created an
    [issue](https://github.com/colbyfayock/netlify-plugin-cloudinary/issues)
    ticket for this PR
    - [ ] I have checked to ensure there aren't other open [Pull
    Requests](https://github.com/colbyfayock/netlify-plugin-cloudinary/pulls)
    for the same update/change?
    - [ ] I have performed a self-review of my own code
    - [ ] I have run tests locally to ensure they all pass
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes needed to the documentation

--- netlify-plugin-cloudinary/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  setupFiles: ['dotenv/config','<rootDir>/.setTestEnvVars.js'],
-  testPathIgnorePatterns: ['./node_modules/', './.netlify'],
-}
