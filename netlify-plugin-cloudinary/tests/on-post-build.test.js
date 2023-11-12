18a2c58fc57d5e5b5ccf0540d028fc67adeb9f49
Gretchen Shelby-Dormer <gretchenesd@gmail.com>
2023-11-12 18:50:34 -0400

    Use negated regex shorthand for word characters to remove illegal characters from paths
    * Fix tests breaking on Windows due to illegal character > in paths

--- netlify-plugin-cloudinary/tests/on-post-build.test.js
+++ netlify-plugin-cloudinary/tests/on-post-build.test.js
@@ -6,6 +6,9 @@ import { onPostBuild } from '../src/';
 
 const mocksPath = path.join(__dirname, 'mocks/html');
 const tempPath = path.join(mocksPath, 'temp');
+// Avoid illegal characters in file paths, all operating systems
+const replaceRegEx = /[\W_]+/g
+const replaceValue = '_'
 
 async function mkdir(directoryPath) {
   let dir;
@@ -36,7 +39,7 @@ describe('onPostBuild', () => {
     process.env.CLOUDINARY_API_SECRET = 'abcd1234';
 
     const mockFiles = (await fs.readdir(mocksPath)).filter(filePath => filePath.includes('.html'));
-    const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace('/', '_'));
+    const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace(replaceRegEx, replaceValue));
     await mkdir(tempTestPath);
     await Promise.all(mockFiles.map(async file => {
       await fs.copyFile(path.join(mocksPath, file), path.join(tempTestPath, file));
@@ -46,7 +49,7 @@ describe('onPostBuild', () => {
   afterEach(async () => {
     process.env = ENV_ORIGINAL;
 
-    await fs.rm(path.join(tempPath, expect.getState().currentTestName.replace('/', '_')), { recursive: true, force: true });
+    await fs.rm(path.join(tempPath, expect.getState().currentTestName.replace(replaceRegEx, replaceValue)), { recursive: true, force: true });
   });
 
   afterAll(async () => {
@@ -66,7 +69,7 @@ describe('onPostBuild', () => {
 
       const deliveryType = 'fetch';
 
-      const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace('/', '_'));
+      const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace(replaceRegEx, replaceValue));
 
       await onPostBuild({
         constants: {
@@ -105,7 +108,7 @@ describe('onPostBuild', () => {
 
       const deliveryType = 'fetch';
 
-      const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace('/', '_'));
+      const tempTestPath = path.join(tempPath, expect.getState().currentTestName.replace(replaceRegEx, replaceValue));
 
       const maxSize = {
         width: 800,
